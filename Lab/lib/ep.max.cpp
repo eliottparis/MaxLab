ec7f3b89a458eb92087e5318a20b3bd8685783d8
eliott paris <eliottparis@gmail.com>
2014-03-23 15:43:52 +0100

    timestretch~ project

--- Lab/lib/ep.max.cpp
+++ Lab/lib/ep.max.cpp
@@ -1,8 +1,8 @@
-/*
-// Copyright (c) 2012-2014 Eliott Paris, Julien Colafrancesco & Pierre Guillot, CICM, Universite Paris 8.
-// For information on usage and redistribution, and for a DISCLAIMER OF ALL
-// WARRANTIES, see the file, "LICENSE.txt," in this distribution.
-*/
+/**
+ * Copyright (c) 2014 Eliott Paris
+ * For information on usage and redistribution, and for a DISCLAIMER OF ALL
+ * WARRANTIES, see the file, "LICENSE" in this distribution.
+ */
 
 #include "ep.max.h"
 
